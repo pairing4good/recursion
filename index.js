abdfb9d32d283d1b37283e8719002bdd91d08dbb
David Kessler <39646674+pairing4good@users.noreply.github.com>
2024-01-18 16:11:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+function markAsDone(todos) {
+    return transform(0, todos, []);
+}
+
+function transform(index, todos, modifiedTodos){
+    if(index < todos.length){
+        modifiedTodos.push('done - ' + todos[index]);
+        transform(++index, todos, modifiedTodos);
+        return modifiedTodos
+    }else{
+        return modifiedTodos
+    }
+}
